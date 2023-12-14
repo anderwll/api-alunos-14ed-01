3afd8c9ee3130f69b9973cb06e9e7c822679fa91
leandrofn3 <lf1938833@gmail.com>
2023-12-14 20:41:42 -0300

    add arquivo swagger docs

--- src/docs/docs.routes.ts
+++ src/docs/docs.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { serve, setup } from 'swagger-ui-express'
+import swaggerDocs from '.';
 
 const router = Router()
 
-router.use('/docs', serve, setup());
+router.use('/docs', serve, setup(swaggerDocs));
 
 export default router
---
+++ src/docs/index.ts
@@ -0,0 +1,24 @@
+const swaggerDocs = {
+  openapi: "3.0.3",
+  info: {
+    title: "API de Alunos - Growdev",
+    description: "Sistema de gerenciamento de alunos e suas avaliações",
+    contact: {
+      email: "growdev@growdev.com",
+    },
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3333",
+    },
+    {
+        url: "http://localhost:8080",
+      },
+  ],
+  paths: {
+
+  }
+};
+
+export default swaggerDocs;
