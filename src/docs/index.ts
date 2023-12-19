087d9767a79beb2f3d2fe07db04a533ddbb4d5b9
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-12-18 22:57:56 -0300

    initial doc assessments

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -2,7 +2,7 @@ const swaggerDocs = {
   openapi: "3.0.3",
   info: {
     title: "API de Alunos - Growdev",
-    description: "Sistema de gerenciamento de alunos e suas avaliações",
+    description: "Sistema de gerenciamento de alunos e suas avaliações.",
     contact: {
       email: "growdev@growdev.com",
     },
@@ -20,6 +20,10 @@ const swaggerDocs = {
     {
       name: "Usuários",
       description: "Usuários da aplicação",
+    },
+    {
+      name: "Avaliações",
+      description: "Avaliações dos usuários cadastrados na aplicação",
     },
   ],
   paths: {
